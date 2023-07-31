b50847271eb3baa29100ec42416c94da53922a7c
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-07-31 09:38:45 -0700

    rename workflow

--- .github/workflow_scripts/env_setup.sh
+++ .github/workflow_scripts/env_setup.sh
@@ -1,4 +1,4 @@
-function setup_lint_env {
+function setup_env {
     python3 -m pip install --upgrade pip
     python3 -m pip install -r requirements.txt
 }
--- .github/workflow_scripts/link_check.sh
+++ .github/workflow_scripts/link_check.sh
@@ -4,6 +4,6 @@ set -ex
 
 source $(dirname "$0")/env_setup.sh
 
-setup_lint_env
+setup_env
 
 python3 get_broken_links.py
